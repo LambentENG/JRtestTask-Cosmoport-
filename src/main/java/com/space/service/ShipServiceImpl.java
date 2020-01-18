52f4e711f8fac0d101f92354c93a56e9bbb94937
lambenteng <lambenteng@gmail.com>
2020-01-19 02:12:46 +0300

    testTask 1.0.5 - ShipServiceImpl - part 2

--- src/main/java/com/space/service/ShipServiceImpl.java
+++ src/main/java/com/space/service/ShipServiceImpl.java
@@ -3,14 +3,19 @@ package com.space.service;
 import com.space.exceptions.BadRequestException;
 import com.space.exceptions.ShipNotFoundException;
 import com.space.model.Ship;
+import com.space.model.ShipType;
 import com.space.repository.ShipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Calendar;
-
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class ShipServiceImpl implements ShipService {
@@ -49,13 +54,26 @@ public class ShipServiceImpl implements ShipService {
         }
     }
 
-@Override
+    @Override
     public Long idChecker(String id) {
-//realization is to be added
+        if (id == null || id.equals("0") || id.equals("")) {
+            throw new BadRequestException("ID is incorrect");
+        }
+        try {
+            Long iD = Long.parseLong(id);
+            return iD;
+        } catch (NumberFormatException e) {
+            throw new BadRequestException("ID is not a number", e);
+        }
     }
 
     private Double calculateRating(Ship shipRequired) {
-        //realization to be added
+        Calendar date = Calendar.getInstance();
+        date.setTime(shipRequired.getProdDate());
+        int year = date.get(Calendar.YEAR);
+        BigDecimal rating = new BigDecimal((80 * shipRequired.getSpeed() * (shipRequired.getUsed() ? 0.5 : 1)) / (3019 - year + 1));
+        rating = rating.setScale(2, RoundingMode.HALF_UP);
+        return rating.doubleValue();
     }
 
     @Override
@@ -132,9 +150,113 @@ public class ShipServiceImpl implements ShipService {
 
     @Override
     public void updateShip(Long id) {
-//optional
+        //optional
     }
 
-//Specification is to be added
+    @Override
+    public List<Ship> getAllExistingShipsList(Specification<Ship> specification) {
+        return shipRepository.findAll(specification);
+    }
 
+    @Override
+    public Page<Ship> getAllExistingShipsList(Specification<Ship> specification, Pageable sortedByName) {
+        return shipRepository.findAll(specification, sortedByName);
+    }
+
+    @Override
+    public Specification<Ship> nameFilter(String name) {
+        return (root, query, criteriaBuilder) -> name == null ? null : criteriaBuilder.like(root.get("name"), "%" + name + "%");
+    }
+
+    @Override
+    public Specification<Ship> planetFilter(String planet) {
+        return (root, query, criteriaBuilder) -> planet == null ? null : criteriaBuilder.like(root.get("planet"), "%" + planet + "%");
+    }
+
+    @Override
+    public Specification<Ship> shipTypeFilter(ShipType shipType) {
+        return (root, query, criteriaBuilder) -> shipType == null ? null : criteriaBuilder.equal(root.get("shipType"), shipType);
+    }
+
+    @Override
+    public Specification<Ship> dateFilter(Long after, Long before) {
+        return (root, query, criteriaBuilder) -> {
+            if (after == null && before == null) {
+                return null;
+            }
+            if (after == null) {
+                Date before1 = new Date(before);
+                return criteriaBuilder.lessThanOrEqualTo(root.get("prodDate"), before1);
+            }
+            if (before == null) {
+                Date after1 = new Date(after);
+                return criteriaBuilder.greaterThanOrEqualTo(root.get("prodDate"), after1);
+            }
+            Date before1 = new Date(before - 3600001);
+            Date after1 = new Date(after);
+            return criteriaBuilder.between(root.get("prodDate"), after1, before1);
+        };
+    }
+
+    @Override
+    public Specification<Ship> usageFilter(Boolean isUsed) {
+        return (root, query, criteriaBuilder) -> {
+            if (isUsed == null) {
+                return null;
+            }
+            if (isUsed) {
+                return criteriaBuilder.isTrue(root.get("isUsed"));
+            } else {
+                return criteriaBuilder.isFalse(root.get("isUsed"));
+            }
+        };
+    }
+
+    @Override
+    public Specification<Ship> speedFilter(Double min, Double max) {
+        return (root, query, criteriaBuilder) -> {
+            if (min == null && max == null) {
+                return null;
+            }
+            if (min == null) {
+                return criteriaBuilder.lessThanOrEqualTo(root.get("speed"), max);
+            }
+            if (max == null) {
+                return criteriaBuilder.greaterThanOrEqualTo(root.get("speed"), min);
+            }
+            return criteriaBuilder.between(root.get("speed"), min, max);
+        };
+    }
+
+    @Override
+    public Specification<Ship> crewSizeFilter(Integer min, Integer max) {
+        return (root, query, criteriaBuilder) -> {
+            if (min == null && max == null) {
+                return null;
+            }
+            if (min == null) {
+                return criteriaBuilder.lessThanOrEqualTo(root.get("crewSize"), max);
+            }
+            if (max == null) {
+                return criteriaBuilder.greaterThanOrEqualTo(root.get("crewSize"), min);
+            }
+            return criteriaBuilder.between(root.get("crewSize"), min, max);
+        };
+    }
+
+    @Override
+    public Specification<Ship> ratingFilter(Double min, Double max) {
+        return (root, query, criteriaBuilder) -> {
+            if (min == null && max == null) {
+                return null;
+            }
+            if (min == null) {
+                return criteriaBuilder.lessThanOrEqualTo(root.get("rating"), max);
+            }
+            if (max == null) {
+                return criteriaBuilder.greaterThanOrEqualTo(root.get("rating"), min);
+            }
+            return criteriaBuilder.between(root.get("rating"), min, max);
+        };
+    }
 }
